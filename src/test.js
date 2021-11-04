b887354551788744254361806ab1340a347f3f9b
Zay Min Maw <zayminmaw77@gmail.com>
2021-11-04 23:59:13 +0630

    tested create new transaction

--- src/test.js
+++ src/test.js
@@ -4,4 +4,16 @@ const genesis = new Blockchain();
 
 genesis.createNewBlock(5424, "PreviousHash", "Hash");
 
+genesis.createNewTransaction(100, "Zay", "Shine");
+
+genesis.createNewBlock(3742, "PreviousHash", "Hash");
+
+genesis.createNewTransaction(50, "Zay", "Shine");
+
+genesis.createNewTransaction(100, "Zay", "Shine");
+
+genesis.createNewTransaction(300, "Zay", "Shine");
+
+genesis.createNewBlock(9751, "PreviousHash", "Hash");
+
 console.log(genesis);
