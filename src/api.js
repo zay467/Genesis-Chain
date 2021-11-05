d02b933b6ef64c42b46e8f2d24f4984f24045611
Zay Min Maw <zayminmaw77@gmail.com>
2021-11-05 11:30:44 +0630

    added endpoints skeleton

---
+++ src/api.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var app = express();
+
+app.get("/blockchain", function (req, res) {});
+
+app.post("/transaction", function (req, res) {});
+
+app.get("/mine", function (req, res) {});
+
+app.listen(3000, function () {
+  console.log("Listening on port 3000...");
+});
