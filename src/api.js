cd2961955d437d0e32b7472842ec2815fd7cdb0f
Zay Min Maw <zayminmaw77@gmail.com>
2021-11-05 13:35:53 +0630

    added endpoints

--- src/api.js
+++ src/api.js
@@ -1,11 +1,42 @@
-var express = require("express");
-var app = express();
+const express = require("express");
+const bodyParser = require("body-parser");
+const Blockchain = require("./blockchain.js");
+const uuid = require("uuid/v1");
 
-app.get("/blockchain", function (req, res) {});
+const nodeAddress = uuid().split("-").join("");
 
-app.post("/transaction", function (req, res) {});
+const genesis = new Blockchain();
+const app = express();
 
-app.get("/mine", function (req, res) {});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/blockchain", function (req, res) {
+  res.send(genesis);
+});
+
+app.post("/transaction", function (req, res) {
+  const blockIndex = genesis.createNewTransaction(
+    req.body.amount,
+    req.body.sender,
+    req.body.recipient
+  );
+  res.json({ note: `Transaction will be added in block ${blockIndex}.` });
+});
+
+app.get("/mine", function (req, res) {
+  const lastBlock = genesis.getLastBlock();
+  const previousBlockHash = lastBlock["hash"];
+  const currentBlockData = {
+    transactions: genesis.pendingTransactions,
+    index: lastBlock["index"] + 1,
+  };
+  const nonce = genesis.proofOfWork(previousBlockHash, currentBlockData);
+  const hash = genesis.hashBlock(previousBlockHash, currentBlockData, nonce);
+  genesis.createNewTransaction(10, "00", nodeAddress);
+  const newBlock = genesis.createNewBlock(nonce, previousBlockHash, hash);
+  res.json({ note: "New block mined successfully.", block: newBlock });
+});
 
 app.listen(3000, function () {
   console.log("Listening on port 3000...");
